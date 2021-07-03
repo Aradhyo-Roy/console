99497f2393b01b76ba872939c0109deaba6e2ab7
Aradhyo-Roy <83859573+Aradhyo-Roy@users.noreply.github.com>
2021-07-03 15:22:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+console.log("Hi my name is Aradhyo roy");
+
+var first_name="Aradhyo";
+console.log(first_name);
+
+var last_name="Roy";
+console.log(last_name);
+
+var full_name=first_name.concat(last_name);
+console.log(full_name);
+
+var how_many_words_are_there_in_the_full_name=full_name.length;
+console.log(how_many_words_are_there_in_the_full_name);
+
+var name_one="I am Aradhyo roy";
+
+var replace_one=name_one.replace("roy","mazumdar");
+console.log(replace_one);
+
+var a=6;
+var b=12;
+var c=18;
+
+var add=a+b+c;
+console.log(add);
+
+var subtract=a-c;
+console.log(subtract);
+
+var subtract_2=a-b;
+console.log(subtract_2);
+
+var subtract_3=b-c;
+console.log(subtract_3);
+
+var multiply=a*c;
+console.log(multiply);
+
+var multiply_2=a*b;
+console.log(multiply_2);
+
+var multiply_3=b*c;
+console.log(multiply_3);
+
+var divide=a/b;
+console.log(divide);
+
+var divide_2=a/c;
+console.log(divide_2);
+
+var divide_3=b/c;
+console.log(divide_3);
